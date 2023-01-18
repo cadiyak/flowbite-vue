4ab12dce205a38a6ca290fdedb1525de3b3efc9f
Adrien Cosson <adrien.cosson.dev@gmail.com>
2023-01-18 16:32:10 +0100

    feat: Working pagination component.

--- docs/components/pagination/examples/PaginationExample.vue
+++ docs/components/pagination/examples/PaginationExample.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="vp-raw flex flex-col">
-    <Pagination @goToPage="ici($event)" :currentPage="currentPage"></Pagination>
+    <Pagination @goToPage="goToPage($event)" @goToPreviousPage="goToPreviousPage" @goToNextPage="goToNextPage" :currentPage="currentPage" :totalPages="totalPages"></Pagination>
   </div>
-  {{ currentPage }}
 </template>
 <script setup>
 import { Pagination } from '../../../../src/index'
 import { ref } from 'vue'
 
 const currentPage = ref(1)
+const totalPages = 6
 
-const ici = (p) => {
+const goToPage = (p) => {
   currentPage.value = p
-  alert(currentPage.value)
 }
+
+const goToPreviousPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value --
+  }
+}
+const goToNextPage = () => {
+  if (currentPage.value < totalPages) {
+    currentPage.value ++
+  }
+}
+
 </script>
--- src/components/Pagination/Pagination.vue
+++ src/components/Pagination/Pagination.vue
@@ -2,12 +2,12 @@
   <nav aria-label="Page navigation example">
     <ul class="inline-flex -space-x-px">
       <li>
-        <a href="#" class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <a href="#" @click="$emit('goToPreviousPage')" class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           Previous
         </a>
       </li>
       <li v-for="(page, index) in totalPages" @click="$emit('goToPage', pageNumber(index)), goToPage(index)" key="index">
-        <a v-if="pageNumber(index) !== currentPage"  href="#" class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <a v-if="pageNumber(index) != currentPage"  href="#" class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           {{ pageNumber(index) }}
         </a>
         <a v-else href="#" aria-current="page" class="py-2 px-3 text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">
@@ -15,7 +15,7 @@
         </a>
       </li>
       <li>
-        <a href="#" class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <a href="#" @click="$emit('goToNextPage')" class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           Next
         </a>
       </li>
@@ -23,8 +23,6 @@
   </nav>
 </template>
 <script lang="ts" setup>
-import {computed, ref, toRefs } from 'vue'
-import type { PropType } from 'vue'
 
 const props = defineProps({
   currentPage: {
@@ -47,12 +45,6 @@ const props = defineProps({
 
 const pageNumber = (index: number) => {
   return index + 1
-}
-
-const currentPage = ref(props.currentPage)
-
-const goToPage = (index: number) => {
-  currentPage.value = pageNumber(index)
 }
 
 </script>
